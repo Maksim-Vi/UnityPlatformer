23647f03afed460f42015d15096eb6694c5034c3
Maksym Vitman <mvitman@gamepoint.com>
2025-02-10 12:48:37 +0100

    fixed spown system coins

--- Assets/Code/Entity/CollectibleSpawnManager.cs
+++ Assets/Code/Entity/CollectibleSpawnManager.cs
@@ -1,4 +1,6 @@
 
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using Utilits;
 
@@ -7,43 +9,36 @@ namespace Platformer
     public class CollectibleSpawnManager : EntitySpawnManager
     {
         [SerializeField] CollactableData[] collactableData;
-        [SerializeField] float spawnInterval = 1f;
+        [SerializeField] GameObject spawnPlace;
+        [SerializeField] int spawnInterval = 1;
 
         EntitySpawner<Collectible> spawner;
 
         CountdownTimer spawnTimer;
         int counter;
 
-        protected override void Awake() 
+        protected override async void Awake() 
         {
             base.Awake();
 
             spawner = new EntitySpawner<Collectible>(
-                new EntityFactory<Collectible>(collactableData),
+                new EntityFactory<Collectible>(collactableData, spawnPlace),
                 spawnPointStrategy
             );
 
-            spawnTimer = new CountdownTimer(spawnInterval);
-
-            spawnTimer.OnTimerStop += () =>{
-                if(counter++ >= spawnPoints.Length)
-                {
-                    spawnTimer.Stop();
-                    return;
-                }
-            };
-
-            Spawn();
-            counter++;
-            spawnTimer.Start();
+            for (int i = 0; i < spawnPoints.Length; i++)
+            {
+                await UniTask.Delay(spawnInterval);
+                Spawn();
+            }
         }
 
         private void Start() {
-            spawnTimer.Start();
+            //spawnTimer.Start();
         }
 
         private void Update() {
-            spawnTimer.Tick(Time.deltaTime);
+            //spawnTimer.Tick(Time.deltaTime);
         }
 
         public override void Spawn()
--- Assets/Code/Entity/Factory/EntityFactory.cs
+++ Assets/Code/Entity/Factory/EntityFactory.cs
@@ -5,16 +5,18 @@ namespace Platformer
     public class EntityFactory<T> : IEntityFactory<T> where T : Entity
     {
         EntityData[] data;
+        GameObject spawnPlace;
 
-        public EntityFactory(EntityData[] data)
+        public EntityFactory(EntityData[] data, GameObject spawnPlace)
         {
             this.data = data;
+            this.spawnPlace = spawnPlace;
         }
 
         public T Create(Transform spawnPoints)
         {
             EntityData entityData = data[Random.Range(0, data.Length)];
-            GameObject instance = GameObject.Instantiate(entityData.prefab, spawnPoints.position, spawnPoints.rotation);
+            GameObject instance = GameObject.Instantiate(entityData.prefab, spawnPoints.position, spawnPoints.rotation, spawnPlace.transform);
             
             return instance.GetComponent<T>();
         }
